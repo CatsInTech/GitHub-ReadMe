87a99e76b21f46fa0b91ffc5879e816ec8cc99fb
Kapileswar Moharana <kapileswarmoharana32161@gmail.com>
2023-06-17 17:16:45 +0530

    Added a Button For Enhance the Dark mode, Make Changes in App.js and App.Scss

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Header from "./Bootstrap/Header";
 import Home from "./Home";
 import Docs from "./Docs";
 
 class App extends Component {
-  state = {};
+  state = {
+    isDarkMode: false
+  };
+
+  toggleDarkMode = () => {
+    this.setState(prevState => ({
+      isDarkMode: !prevState.isDarkMode
+    }));
+  };
+
   render() {
     const NavItems = [
       <Link className="nav-link" to="/">
@@ -15,17 +24,27 @@ class App extends Component {
         Docs
       </Link>
     ];
+
+    const { isDarkMode } = this.state;
+
     return (
-      <div className="App">
+      <div className={`App ${isDarkMode ? "dark-mode" : ""}`}>
         <Header
           to="/"
           Link={Link}
-          dark={true}
+          dark={isDarkMode}
           items={NavItems}
           className="AppHeader"
         >
           GitHub ReadMe
         </Header>
+        <button className="switch-btn" onClick={this.toggleDarkMode}>
+          {isDarkMode ? (
+            <span>Light Mode</span>
+          ) : (
+            <span>Dark Mode</span>
+          )}
+        </button>
         <Routes>
           <Route path="/Docs" element={<Docs />} />
           <Route path="/" element={<Home />} />
